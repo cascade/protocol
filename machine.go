69723085e8eaf689deb7122c2c28d4a89c3b8f49
Hiromu OCHIAI <otiai10@gmail.com>
2017-12-04 19:06:07 +0900

    Fix decode tags

--- machine.go
+++ machine.go
@@ -3,9 +3,9 @@ package protocol
 // Machine defines on which machine "Job" is gonna be handled.
 type Machine struct {
 	// Provider specifies machine provider, either of [local, ec2, gce, k8s]
-	Provider string `json:"provider" yaml:"steps"`
+	Provider string `json:"provider" yaml:"provider"`
 	// CPU specifies how many CPU cores are required for the "Job"
-	CPU int `json:"cpu" yaml:"cpu"`
+	CPU int `json:"cpu,omitempty" yaml:"cpu,omitempty"`
 	// Memory specifies how much memory are required for the "Job"
-	Memory string `json:"memory" yaml:"memory"`
+	Memory string `json:"memory,omitempty" yaml:"memory,omitempty"`
 }
